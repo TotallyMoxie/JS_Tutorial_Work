15dc6961a22e58d62c44d1e13adca132f815aca5
TotallyMoxie <134233554+TotallyMoxie@users.noreply.github.com>
2023-06-19 19:38:57 -0500

    js tutorial at If Statements

---
+++ js/main.js
@@ -0,0 +1,23 @@
+console.log("Shaina".charAt());
+console.log(Math.floor(Math.random() * 4 + 1));
+
+console.log("Shaina".charAt(Math.random()));
+console.log("Shaina".charAt(Math.random()));
+console.log("Shaina".charAt(Math.random()));
+console.log("Shaina".charAt(Math.floor(Math.random() * 4 + 1)));
+console.log("Shaina".charAt(Math.floor(Math.random())));
+console.log("Shaina".charAt(Math.floor(Math.random() * 5)));
+
+const anyName = "Jonathan";
+
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+
+
+//Conditionals: If Statements
+
+//syntax
+if (condition) {
+  //run some code
+} else {
+  //run some different code
+}
