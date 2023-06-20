caaed4282474153021a43783aca29088d1317dd8
TotallyMoxie <134233554+TotallyMoxie@users.noreply.github.com>
2023-06-20 18:27:36 -0500

    rock paper scissors full

--- js/main.js
+++ js/main.js
@@ -214,3 +214,64 @@ let result =
     ? "Computer wins!"
     : "playerOne wins!";
 console.log(result);
+
+
+
+
+
+// User Input
+
+/* alert("Hello World!") */
+/* let myBoolean = confirm("Ok === True\nCancel === False");
+console.log(myBoolean); */
+/* let name = prompt("Please enter your name.");
+if (name) {
+    console.log(name.length);
+    console.log(name.trim().length);
+  console.log(name.trim());
+} else {
+  console.log("You didn't enter your name.");
+} */
+
+
+
+
+
+
+// Your First Interactive Game
+
+let playGame = confirm("Shall we play rock, paper, or scissors?");
+if (playGame) {
+  //play
+  let playerChoice = prompt("Please enter rock, paper, or scissors.");
+  if (playerChoice) {
+    let playerOne = playerChoice.trim().toLowerCase();
+    if (playerOne == "rock" || playerOne === "paper" || playerOne === "scissors"){
+
+      let computerChoice = Math.floor(Math.random() * 3 + 1);
+      let computer = computerChoice === 1 ? "rock"
+        : computerChoice === 2 ? "paper"
+        : "scissors";
+
+      let result =
+      playerOne === computer
+      ? "Tie game!"
+      : playerOne === "rock" && computer === "paper"
+      ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+      : playerOne === "paper" && computer === "scissors"
+      ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+      : playerOne === "scissors" && computer === "rock"
+      ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+      : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`;
+    alert(result);
+    let playAgain = confirm("Play Again?");
+    playAgain ? location.reload() : alert("Ok, thanks for playing.");
+    }else {
+      alert("You didn't enter rock, paper, or scissors.");
+    }
+  } else {
+    alert("I guess you changed your mind, maybe next time.");
+  }
+}else {
+  alert("Ok, maybe next time.");
+}
