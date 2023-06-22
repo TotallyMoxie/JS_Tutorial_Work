7cd84b1d98a07127a8b58106c40654bcbde682ca
TotallyMoxie <134233554+TotallyMoxie@users.noreply.github.com>
2023-06-22 17:39:14 -0500

    objects final

--- js/main.js
+++ js/main.js
@@ -506,19 +506,40 @@ console.log(anotherObj.action());
  */
 
 // objects--inheritance
-const vehicle = {
+/* const vehicle = {
   wheels: 4,
   engine: function() {
     return "Vrroooom!";
   }
-}
-
-const truck = Object.create(vehicle);
-truck.doors = 2;
-console.log(truck);
-console.log(truck.wheels);
-console.log(truck.engine());
+};
 const car = Object.create(vehicle);
 car.doors = 4;
 car.engine = function() { return "Whooooosh!"};
 console.log(car.engine());
+console.log(car.wheels);
+const tesla = Object.create(car);
+console.log(tesla.wheels);
+tesla.engine = function() {return "Shhhh...."};
+console.log(tesla.engine()); */
+
+const band = {
+  vocals: "Robert Plant",
+  guitar: "Jimmy Page",
+  bass: "John Paul Jones",
+  drums: "John Bonham"
+};
+
+/* delete band.drums;
+console.log(band.hasOwnProperty("drums"));
+
+console.log(Object.keys(band));
+console.log(Object.values(band));
+
+for (let job in band) {
+  console.log(`On ${job}, it's ${band[job]}!`);
+} */
+
+// destructuring objects
+const { vocals, guitar, bass, drums } = band;
+console.log(guitar);
+console.log(vocals);
