cac2f003cec69ca6b12a8b623d36553b5f40ba84
TotallyMoxie <134233554+TotallyMoxie@users.noreply.github.com>
2023-06-24 09:38:17 -0500

    event selector end

--- js/main.js
+++ js/main.js
@@ -676,7 +676,7 @@ console.log(evenDivs[0].nextElementSibling);
 console.log(evenDivs[0].previousSibling);
 console.log(evenDivs[0].previousElementSibling); */
 
-// ADDING AND REMOVING FROM DOM
+ // ADDING AND REMOVING FROM DOM
 /* view1.style.display = "none";
 view2.style.display = "flex";
 view2.style.flexDirection = "row";
@@ -698,12 +698,12 @@ const createDivs = (parent, iter) => {
   newDiv.style.justifyContent = "center";
   newDiv.style.alignItems = "center";
   parent.append(newDiv);
-} */
-// createDivs(view2,10);
-/* for (let i = 1; i <= 12; i++) {
+} 
+ createDivs(view2,10);
+ for (let i = 1; i <= 12; i++) {
   createDivs(view2, i);
-} */
-
+} 
+ 
 
 
 
@@ -718,3 +718,74 @@ const createDivs = (parent, iter) => {
 
 
 // Event Listeners
+
+// Syntax: addEventListener(event, function, useCapture)
+
+/* const doSomething = () => {
+  alert("doing something");
+}
+
+h2.addEventListener("click", doSomething, false);
+h2.removeEventListener("click", doSomething, false);
+
+h2.addEventListener("click", (event) => {
+  console.log(event.target);
+  event.target.textContent = "Clicked";
+}); */
+
+// ready state change event
+/* document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    console.log("readyState: complete");
+    initApp();
+  }
+});
+
+const initApp = () => {
+  const view2 = document.querySelector("#view2");
+  const div = view.querySelector("div");
+  const h2 = div.querySelector("h2");
+  
+  view.addEventListener("click", (event) => {
+    view.classList.toggle("purple");
+    view.classList.toggle("darkblue");
+  }, false );
+
+  div.addEventListener("click", (event) => {
+    div.classList.toggle("blue");
+    div.classList.toggle("black");
+  }, false );
+
+  h2.addEventListener("click", (event) => {
+    const myText = event.target.textContent;
+    myText === "My 2nd View" 
+      ? (event.target.textContent = "Clicked")
+      : (event.target.textContent = "My 2nd View");
+  }, 
+  false 
+  );
+
+  const nav = document.querySelector("nav");
+  nav.addEventListener("mouseover", (event) => {
+    event.target.classList.add("height100");
+  });
+  nav.addEventListener("mouseout", (event) => {
+    event.target.classList.remove("height100");
+  });
+}; */
+
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    console.log("readyState: complete");
+    initApp();
+  }
+});
+
+const initApp = () => {
+  const view3 = document.querySelector("#view3");
+  const myForm = view3.querySelector("#myForm");
+  myForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("submit event");
+  });
+};
