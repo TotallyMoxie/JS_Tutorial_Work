f741d2dfefe3b5bd0b11cc0ae92ca949e7fd4017
TotallyMoxie <134233554+TotallyMoxie@users.noreply.github.com>
2023-06-23 20:32:16 -0500

    event selectors

--- js/main.js
+++ js/main.js
@@ -633,15 +633,10 @@ console.log(typeof receiveJSON); */
 
 
 // Document Object Model (DOM)
-const view1 = document.getElementById("view1");
-console.log(view1);
-const view2 = document.querySelector("#view2");
-console.log(view2);
-view1.style.display = "flex";
-view2.style.display = "flex";
+/*  */
 //allows to scroll between view 1 and 2
 
-const views = document.getElementsByClassName("view");
+/* const views = document.getElementsByClassName("view");
 console.log(views);
 const sameViews = document.querySelectorAll('.view');
 console.log(sameViews);
@@ -652,14 +647,14 @@ const sameDivs = view1.getElementsByTagName("div");
 console.log(sameDivs);
 
 const evenDivs = view1.querySelectorAll("div:nth-of-type(2n)");
-console.log(evenDivs);
-for (let i = 0; i < evenDivs.length; i++) {
-  evenDivs[i].style.backgroundColor = "darkblue";
+console.log(evenDivs); */
+//for (let i = 0; i < evenDivs.length; i++) {
+  //evenDivs[i].style.backgroundColor = "darkblue";
   /* evenDivs[i].style.width = "200px";
   evenDivs[i].style.height = "200px"; */
-}
+//}
 
-const navText = document.querySelector("nav h1");
+/* const navText = document.querySelector("nav h1");
 console.log(navText);
 navText.textContent = "Hello World!";
 const navbar = document.querySelector("nav");
@@ -679,10 +674,10 @@ console.log(evenDivs[0].parentElement.firstElementChild);
 console.log(evenDivs[0].nextSibling);
 console.log(evenDivs[0].nextElementSibling);
 console.log(evenDivs[0].previousSibling);
-console.log(evenDivs[0].previousElementSibling);
+console.log(evenDivs[0].previousElementSibling); */
 
 // ADDING AND REMOVING FROM DOM
-view1.style.display = "none";
+/* view1.style.display = "none";
 view2.style.display = "flex";
 view2.style.flexDirection = "row";
 view2.style.flexWrap = "wrap";
@@ -703,8 +698,23 @@ const createDivs = (parent, iter) => {
   newDiv.style.justifyContent = "center";
   newDiv.style.alignItems = "center";
   parent.append(newDiv);
-}
+} */
 // createDivs(view2,10);
-for (let i = 1; i <= 12; i++) {
+/* for (let i = 1; i <= 12; i++) {
   createDivs(view2, i);
-}
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Event Listeners
